582c6e43202de4b9a473266f325308e4f3c91b86
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-07 16:54:56 -0400

    Adjust comment to explain expected info from client for the route

--- src/players/player.route.js
+++ src/players/player.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// gets all players in sorted order from best score to worse
+// gets all players in sorted order from best score to worse.
+// this route expects client to provide a page number and how
+// many documents/items go on each page.
 router.get("/");
 
 // creates a new player
