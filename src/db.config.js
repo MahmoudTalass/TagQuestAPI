555ef6a5412cd39ceb5c7b51b3c9e990002753ed
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-06 22:27:16 -0400

    Initial express project setup

---
+++ src/db.config.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+connectDB().catch((err) =>
+   app.status(500).json({ status: 500, message: "Something went wrong. Please try again later" })
+);
+
+async function connectDB() {
+   await mongoose.connect(process.env.DB_URI);
+}
