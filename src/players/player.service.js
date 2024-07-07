837489207ff6b0a0c118d873bb6d2e5d7b3c43ef
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-07 16:59:45 -0400

    Create player service to interact with Player collection
    Used to create, get a certain player, and get an array of players.

---
+++ src/players/player.service.js
@@ -0,0 +1,27 @@
+const Player = require("./player.model");
+
+class PlayerService {
+   async getPlayers(pageNumber, playersPerPage) {
+      const players = await Player.find({})
+         .skip(pageNumber * playersPerPage)
+         .limit(playersPerPage)
+         .exec();
+      return players;
+   }
+
+   async getPlayer(id) {
+      const player = await Player.findById(id).exec();
+      return player;
+   }
+
+   async createPlayer(name, score) {
+      const player = new Player({
+         name,
+         score,
+      });
+
+      return await player.save();
+   }
+}
+
+module.exports = PlayerService;
