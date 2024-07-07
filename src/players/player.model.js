10f965d344dc180e4320631a2bafe0bc419c36ec
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-07 17:10:25 -0400

    Expect score to be a number since it will be a number of seconds

--- src/players/player.model.js
+++ src/players/player.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PlayerSchema = new Schema({
    name: { type: String, required: true },
-   score: { type: String, required: true },
+   score: { type: Number, required: true, min: 1 },
 });
 
 module.exports = mongoose.model("Player", PlayerSchema);
