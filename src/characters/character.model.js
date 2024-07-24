cd1aef639915a4bd10b629617481e0b51ed30e6b
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-24 19:10:03 -0400

    Make it so x and y range arrays store numbers

--- src/characters/character.model.js
+++ src/characters/character.model.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 const CharacterSchema = new Schema({
    name: { type: String, required: true },
    xLocationRange: {
-      type: [{ type: Array, required: true }],
+      type: [{ type: Number, required: true }],
       validate: [arrayLength, "{PATH} must contain the starting x and the ending x ranges."],
    },
    yLocationRange: {
-      type: [{ type: Array, required: true }],
+      type: [{ type: Number, required: true }],
       validate: [arrayLength, "{PATH} must contain the starting y and the ending y ranges."],
    },
    image: { type: Schema.Types.Buffer, required: true },
