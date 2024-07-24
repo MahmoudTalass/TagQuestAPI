d94bb15b17233584ebf0847018a0797d159fba52
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-24 19:17:58 -0400

    Attach controller functions to player routes

--- src/players/player.route.js
+++ src/players/player.route.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const playerController = require("./player.controller");
 
 // gets all players in sorted order from best score to worse.
 // this route expects client to provide a page number and how
 // many documents/items go on each page.
-router.get("/");
+router.get("/", playerController.getPlayers);
 
 // creates a new player
-router.post("/");
-
-// get a certain player
-router.get("/:id");
+router.post("/", playerController.createPlayer);
 
 module.exports = router;
