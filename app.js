87955f13e2486e28c99ceabf8bfc092a0a786c54
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-24 19:14:45 -0400

    Require index route to access all routes and use cors

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 require("./src/db.config");
+app.use(cors());
+
+const indexRouter = require("./src/index.route");
+
+app.use("/api", indexRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
