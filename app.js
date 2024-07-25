8fb64a2f6eb94982c91277d6da9ef574764ca522
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-25 17:57:34 -0400

    Accept json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+app.use(express.json());
 
 require("./src/db.config");
 app.use(cors());
