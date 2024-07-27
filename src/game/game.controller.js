bed6a418842fc7fa744d6576bb913938555a3f24
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-27 09:08:35 -0400

    Validate user input for checking attempts

--- src/game/game.controller.js
+++ src/game/game.controller.js
@@ -2,6 +2,8 @@ const asyncHandler = require("express-async-handler");
 const GameService = require("./game.service");
 const jwt = require("jsonwebtoken");
 const AppError = require("../errors/app_errors");
+const { validationResult, body } = require("express-validator");
+const { isValidObjectId } = require("mongoose");
 
 const startGame = asyncHandler(async (req, res, next) => {
    const token = await GameService.createGameJWT();
@@ -9,39 +11,63 @@ const startGame = asyncHandler(async (req, res, next) => {
    res.json({ token });
 });
 
-const checkAttempt = asyncHandler(async (req, res, next) => {
-   const { characterId, token, xLocationRange, yLocationRange } = req.body;
+const checkAttempt = [
+   body("characterId", "Character not found").custom((val) => isValidObjectId(val)),
+   body("token").isJWT(),
+   body(
+      "xLocationRange",
+      "Must provide an array with the starting x range and the ending x range"
+   ).isArray({ max: 2, min: 2 }),
+   body(
+      "yLocationRange",
+      "Must provide an array with the starting y range and the ending y range"
+   ).isArray({ max: 2, min: 2 }),
+   asyncHandler(async (req, res, next) => {
+      const errors = validationResult(req);
 
-   const jsonToken = jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) throw new AppError("Invalid token", 400);
-
-      return decoded;
-   });
-
-   const isCorrectAttempt = GameService.checkAttempt(characterId, xLocationRange, yLocationRange);
-
-   let responseToken;
-   const tokenPayload = {};
-   let hasWon = false;
-
-   if (isCorrectAttempt) {
-      const characterIds = jsonToken.characterIds.filter((charId) => charId != characterId);
-      tokenPayload.characterIds = characterIds;
-      if (characterIds.length === 0) {
-         hasWon = true;
-         tokenPayload.finishTime = Date.now();
+      if (!errors.isEmpty()) {
+         const errorString = errors.array().join("\n");
+         throw new AppError(errorString, 400);
       }
 
-      responseToken = jwt.sign(tokenPayload, process.env.JWT_SECRET);
-   } else {
-      responseToken = token;
-   }
+      const { characterId, token, xLocationRange, yLocationRange } = req.body;
 
-   res.json({
-      hasWon,
-      token: responseToken,
-   });
-});
+      const jsonToken = jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+         if (err) throw new AppError("Invalid token", 400);
+
+         return decoded;
+      });
+
+      const isCorrectAttempt = await GameService.checkAttempt(
+         characterId,
+         xLocationRange,
+         yLocationRange
+      );
+
+      let responseToken;
+      const tokenPayload = {};
+      let hasWon = false;
+
+      if (isCorrectAttempt) {
+         const characterIds = jsonToken.characterIds.filter((charId) => charId != characterId);
+         tokenPayload.characterIds = characterIds;
+         if (characterIds.length === 0) {
+            hasWon = true;
+            tokenPayload.finishTime = Date.now();
+         }
+         tokenPayload.startTime = jsonToken.startTime;
+         responseToken = responseToken = jwt.sign(tokenPayload, process.env.JWT_SECRET);
+      } else {
+         responseToken = token;
+      }
+
+      res.json({
+         hasWon,
+         hitTarget: isCorrectAttempt,
+         token: responseToken,
+      });
+   }),
+];
 
 module.exports = {
    startGame,
