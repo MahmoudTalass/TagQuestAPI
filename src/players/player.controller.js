4bdf2055559b884ff6e27dbec6a30368edad3ed0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-27 09:10:03 -0400

    Validate user input and only take score provided via the JWT

--- src/players/player.controller.js
+++ src/players/player.controller.js
@@ -1,11 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const PlayerService = require("./player.service");
+const { validationResult, body } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const AppError = require("../errors/app_errors");
 
 const getPlayers = asyncHandler(async (req, res, next) => {
    const { page, pageSize } = req.query;
    const result = await PlayerService.getPlayers(page, pageSize);
 
-   res.json({ players: result });
+   res.json(result);
 });
 
 const getPlayer = asyncHandler(async (req, res, next) => {
@@ -14,11 +17,46 @@ const getPlayer = asyncHandler(async (req, res, next) => {
    res.json(player);
 });
 
-const createPlayer = asyncHandler(async (req, res, next) => {
-   const { name, score } = req.body;
-   const player = await PlayerService.createPlayer(name, score);
-   res.json(player);
-});
+const createPlayer = [
+   body("name", "Must provide name with at least 1 character")
+      .trim()
+      .notEmpty()
+      .isString()
+      .escape(),
+   body("token", "Invalid token").isJWT(),
+   asyncHandler(async (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+         const errorString = errors
+            .array()
+            .map((err) => {
+               return err.msg;
+            })
+            .join("\n");
+
+         console.log(errorString);
+         console.log(errorString);
+         throw new AppError(errorString, 400);
+      }
+
+      const { name, token } = req.body;
+
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+         if (err) throw AppError("Invalid token", 400);
+
+         return decoded;
+      });
+
+      const score = Math.floor(
+         (new Date(decodedToken.finishTime).getTime() -
+            new Date(decodedToken.startTime).getTime()) /
+            1000
+      );
+      const player = await PlayerService.createPlayer(name, score);
+      res.json(player);
+   }),
+];
 
 module.exports = {
    getPlayers,
